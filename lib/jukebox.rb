fb81577ccab3b362722138c46195aa62b170df31
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-01 03:46:08 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,4 @@
+require 'pry'
 songs = [
   "Phoenix - 1901",
   "Tokyo Police Club - Wait Up",
@@ -9,4 +10,31 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
 
+def list(songs)
+  i = 0
+  songs.each do |x|
+    puts "#{i+1}. #{songs[i]}"
+    i+=0
+  end
+end
+
+def play(songs)
+  numbered_choices = []
+  numbered_choices << (1..9)
+  
+  puts "Please enter a song name or number:"
+  requested = gets.chomp
+  if songs.include?(requested)
+    
+  elsif (1..9).include?(requested)
+    
+  end
+end
